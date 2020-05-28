4172bce1627e283c4841f67eb798560d0a5b1284
anurag <anurag.sandhu@springrole.com>
2020-05-28 12:16:51 +0530

    Update

--- delete.js
+++ delete.js
@@ -1,31 +1,9 @@
-function isSorted(arr) {
-    let resp = !!arr.reduce((memo, item) => memo && item >= memo && item);
-    return resp;
-}
-function findSmallest(array) {
-    let small = array[0];
-    for (let index = 0; index < array.length; index++) {
-        if (small > array[index]) small = array[index];
-    }
-    return small;
-}
+let a = [1, 2, 3, 4];
+// console.log(a.shift(1));
+console.log(a)
 
-let operations = 0;
-function findNumber(ar) {
-    let outputArray = [];
-    for (let index = 0; index < ar.length; index++) {
-        if (ar[index] > ar[index + 1]) {
-            operations++;
-            outputArray = [...ar.slice(0, index + 1), ar[index + 1] + ar[index + 2], ...ar.slice(index + 3)];
-            break;
-        }
-    }
-    if (!isSorted(outputArray)) {
-        findNumber(outputArray, operations)
-    }
-    return operations;
-}
+console.log(a)
+console.log(a[1].toLocaleString())
 
-console.log(findNumber([1, 6, 2, 3, 4]))
-// console.log(findSmallest([1, 6, 2, 3, 4]))
-// console.log(isSorted([1, 2, 3, 4]))
+
+
